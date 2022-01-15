ac889ee5cd66a997cc833884043619cc29f6b1db
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2022-01-15 14:09:31 +0530

    Delete components directory

--- components/model.py
+++
@@ -1,8 +0,0 @@
-def model(X_train, y_train):
-    
-    from sklearn.neural_network import MLPClassifier
-    
-    model=MLPClassifier(hidden_layer_sizes=(150))
-    model.fit(X_train, y_train)
-
-    return model
